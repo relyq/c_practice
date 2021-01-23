66d2627d568f069dc6e896d04e8dc934f2ea2f72
relyq <nicoale_mainenti@hotmail.es>
2021-01-23 14:47:54 -0300

    hash tables

---
+++ hash_tables/hash_table.c
@@ -0,0 +1,5 @@
+#include "hash_table.h"
+
+// private methods
+
+// public methods
---
+++ hash_tables/hash_table.h
@@ -0,0 +1,10 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include "slist.h"
+
+typedef struct htable{
+
+}htable;
+
+#endif
